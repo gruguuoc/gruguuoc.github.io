0fac6554c7856c76b9b16add78943a205714f81a
gruguuoc <grugu@uoc.edu>
2024-09-01 17:49:49 +0200

    tt

--- scripts/ConnectedUserController.js
+++ scripts/ConnectedUserController.js
@@ -5,23 +5,23 @@ class ConnectedUserController{
      * @param {*} connectedUserData 
      * @description Obtaining the information of the connected user
      */
-  /*  constructor(FB, connectedUserData){
+    constructor(FB, connectedUserData){
         this.fbApi = FB,
         this.connectedUserData = connectedUserData
         this.getUserAccessToken();
-    } */
+    }
 
     /**
      * @description Obtaining the access token of the current connected user
      */
- /*   getUserAccessToken = function(){
+    getUserAccessToken = function(){
         this.accessToken = this.connectedUserData.authResponse.accessToken;
-    }*/
+    }
 
- /*   getUserPages = function(){
+    getUserPages = function(){
         this.fbApi.api(`/me/accounts?access_token=${this.accesToken}`, function(response){
             console.log("Obtained Pages:");
             console.log(response);
         });
-    }*/
+    }
 }
