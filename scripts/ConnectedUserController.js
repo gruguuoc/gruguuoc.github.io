0e38753ecfadbd0f8bbe8f98671820e7a1b88c98
gruguuoc <grugu@uoc.edu>
2024-09-07 20:14:17 +0200

    debug

--- scripts/ConnectedUserController.js
+++ scripts/ConnectedUserController.js
@@ -15,10 +15,10 @@ class ConnectedUserController{
         //The user access token is obtained
         this.getUserAccessToken();
         
-        this.getUserAccounts().then(this.getInstagramBusinessAccount()).then(this.getInstagramMedia()).then(function(data){
-            console.log("data");
-            console.log(data);
-        });
+        this.getUserAccounts()
+            .then(this.getInstagramBusinessAccount())
+            .then(this.getInstagramMedia())
+            .then(this.managePostInformation());
     }
 
     /**
@@ -57,7 +57,7 @@ class ConnectedUserController{
     }
 
     /** 
-     * @description Obtaining the instagram business Id
+     * @description Obtaining the instagram business data (user posts)
     */
     getInstagramMedia = function(){
         const that = this;
@@ -70,5 +70,38 @@ class ConnectedUserController{
                 });
             });
         }
+    }
+
+    /**
+     * 
+     */
+    managePostInformation = function(){
+        const that = this;
+
+        return function(userPost){
+            that.userPosts = data.media.data; //Array
+            const dataPetitions = [];
+            //Finds data about every found post.
+            that.userPosts.forEach(element => {
+                dataPetitions.push(that.getPostInformationById(element));
+            });
+
+            Promise.all(dataPetitions).then(response => {
+                console.log(that.userPosts);
+            })
+        }    
+    }
+
+    /**
+     * @description Obtaining the current number of likes the post has
+     */
+    getPostInformationById = function(post){
+        return new Promise((resolve, reject) => {
+            //Getting the post number of likes
+            that.fbApi.api(`/${post.id}?fields=like_count`, response => {
+                console.log(response);
+                post.likeCount = response.like_count;
+            });
+        });
     }
 }
